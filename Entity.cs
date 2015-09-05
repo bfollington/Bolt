76c388f7ca452f0a1907fda8ca4f5acb8ca599e8
Ben Follington <bfollington@gmail.com>
2015-09-05 12:37:32 +1000

    Entity and World documentation and API expansion

--- Entity.cs
+++ Entity.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System;
 
 public class Entity {
 	
@@ -11,11 +12,21 @@ public class Entity {
 	private List<Entity> children;
 	private Entity parent;
 
+	/// <summary>
+	/// The angle of the Entity in degrees, this is _relative_ to the parent's angle. 
+	/// Underneath, this maps to the z rotation of the transform.
+	/// </summary>
+	/// <value>The angle.</value>
 	public float angle {
 		get { return gameObject.transform.localEulerAngles.z; }
 		set { gameObject.transform.localEulerAngles = new Vector3(0, 0, value); }
 	}
 
+	/// <summary>
+	/// The x position of the Entity, this is _relative_ to the parent's x. 
+	/// Underneath, this maps to the x coordinate of the transform.
+	/// </summary>
+	/// <value>The x position.</value>
 	public float x
 	{
 		get { return gameObject.transform.localPosition.x; }
@@ -28,6 +39,11 @@ public class Entity {
 	    }
 	}
 
+	/// <summary>
+	/// The y position of the Entity, this is _relative_ to the parent's y. 
+	/// Underneath, this maps to the y coordinate of the transform.
+	/// </summary>
+	/// <value>The y position.</value>
 	public float y
 	{
 		get { return gameObject.transform.localPosition.y; }
@@ -40,6 +56,11 @@ public class Entity {
 	    }
 	}
 
+	/// <summary>
+	/// The layer of the Entity, this is _relative_ to the parent's layer. 
+	/// Underneath, this maps to the y coordinate of the transform.
+	/// </summary>
+	/// <value>The layer.</value>
 	public float layer
 	{
 	    get { return gameObject.transform.localPosition.z; }
@@ -53,7 +74,7 @@ public class Entity {
 	}
 
 	public Entity(float x = 0, float y = 0) {
-		gameObject = new GameObject("Test");
+		gameObject = new GameObject(this.GetType().Name);
 		gameObject.SetActive(false);
 
 		this.x = x;
@@ -61,11 +82,23 @@ public class Entity {
 		children = new List<Entity>();
 	}
 
+	/// <summary>
+	/// Get acess to the GameObject that represents this Entity in game.
+	/// </summary>
+	/// <returns>The object.</returns>
 	public GameObject InnerObject() {
 		return gameObject;
 	}
 
-	public Entity AddChild(Entity e) {
+	/// <summary>
+	/// Add one Entity as child of this one. This will add the child
+	/// to the world and run its Added hook.
+	/// Transformations will be relative to the parent from now on.
+	/// </summary>
+	/// <returns>The child that was added.</returns>
+	/// <param name="e">The entity to add.</param>
+	/// <typeparam name="T">The type of Entity to add, optional and used for method chaining.</typeparam>
+	public T AddChild<T>(T e) where T : Entity {
 		children.Add(e);
 		e.parent = this;
 
@@ -73,16 +106,63 @@ public class Entity {
 			e.Add(world);	
 		}
 
+		var x = e.x;
+		var y = e.y;
+
 		e.InnerObject().transform.parent = InnerObject().transform;
 
+		// Preserve the initial coordinates, Unity will try to help and everything will be offset
+		e.x = x;
+		e.y = y;
+
 		return e;
 	}
 
-	public T AddChildTyped<T>(T e) where T : Entity {
-		AddChild(e);
-		return e;
+	/// <summary>
+	/// Gets a child of a particular type.
+	/// </summary>
+	/// <returns>The child.</returns>
+	/// <typeparam name="T">Type of children to find.</typeparam>
+	public T GetChild<T>() where T : Entity {
+		return (T)children.Find( x => x is T );
 	}
 
+	/// <summary>
+	/// Gets the children of a particular type from this Entity.
+	/// Due to a lack of C# wizardry, I can't seem to get the return
+	/// type to be List<T>.
+	/// </summary>
+	/// <returns>The children of type T.</returns>
+	/// <typeparam name="T">Type of children to find.</typeparam>
+	public List<Entity> GetChildren<T>() where T : Entity {
+		return children.FindAll( x => x is T );
+	}
+
+	/// <summary>
+	/// Finds a child using a LINQ query.
+	/// </summary>
+	/// <returns>The child.</returns>
+	/// <param name="match">The query.</param>
+	public Entity FindChild( Predicate<Entity> match ) {
+		return children.Find(match);
+	}
+
+	/// <summary>
+	/// Finds multiple children using a LINQ query.
+	/// </summary>
+	/// <returns>The children.</returns>
+	/// <param name="match">The query.</param>
+	public List<Entity> FindChildren( Predicate<Entity> match ) {
+		return children.FindAll(match);
+	}
+
+	/// <summary>
+	/// Remove a child Entity from this Entity.
+	/// This will remove the child from the world and it will no longer update.
+	///
+	/// </summary>
+	/// <returns>The child that was removed.</returns>
+	/// <param name="e">The child to remove.</param>
 	public Entity RemoveChild(Entity e) {
 		children.Remove(e);
 		e.parent = null;
@@ -94,6 +174,19 @@ public class Entity {
 		e.InnerObject().transform.parent = null;
 
 		return e;
+	}
+
+	/// <summary>
+	/// Remove a list of children from this Entity.
+	/// </summary>
+	/// <returns>The children.</returns>
+	/// <param name="entities">Entities.</param>
+	public List<Entity> RemoveChildren(List<Entity> entities) {
+		foreach (var e in entities) {
+			RemoveChild(e);
+		}
+
+		return entities;
 	}
 
 	public bool IsAdded() {
@@ -112,6 +205,12 @@ public class Entity {
 		return gameObject.AddComponent<T>() as T;
 	}
 
+	/// <summary>
+	/// Called when added to a world, configures the Entity and GameObject
+	/// to start updating.
+	/// Can be invoked manually if neccessary.
+	/// </summary>
+	/// <param name="world">World.</param>
 	public void Add(World world) {
 		this.world = world;
 		this.isAdded = true;
@@ -124,6 +223,12 @@ public class Entity {
 		Added();
 	}
 
+	/// <summary>
+	/// Called when removed from the world. Has the caveat that if a child remove's itself from the world,
+	/// then its parent it re-added, the child will also be re-added.
+	/// Can be invoked manually if neccessary.
+	/// </summary>
+	/// <param name="delay">Optional delay before actually removing the gameObject.</param>
 	public void Remove(float delay = 0) {
 		Removed();
 
--- World.cs
+++ World.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 
 public class World {
 
+	private List<Entity> toRemoveAtEndOfUpdate;
+	private List<Entity> toAddAtEndOfUpdate;
 	private List<Entity> entities;
 	private bool isActive = false;
 
 	public World () {
 		entities = new List<Entity>();
+		toAddAtEndOfUpdate = new List<Entity>();
+		toRemoveAtEndOfUpdate = new List<Entity>();
 	}
 
 	public bool IsActive() {
@@ -30,24 +34,64 @@ public class World {
 		}
 	}
 
+	/// <summary>
+	/// Add the specified entity to the world. 
+	/// Additions are queued until the end of the update cycle.
+	/// </summary>
+	/// <param name="e">Entity.</param>
 	public Entity Add(Entity e) {
-		e.Add(this);
-		entities.Add(e);
+		toAddAtEndOfUpdate.Add(e);
 
 		return e;
 	}
 
+	/// <summary>
+	/// Remove the specified entity to the world. 
+	/// Removals are queued until the end of the update cycle.
+	/// </summary>
+	/// <param name="e">Entity.</param>
 	public Entity Remove(Entity e) {
-		e.Remove();
-		entities.Remove(e);
+		toRemoveAtEndOfUpdate.Add(e);
 
 		return e;
 	}
 
-	public void Update() {
+	private void HandleAddedEntities() {
+		foreach (var e in toAddAtEndOfUpdate) {
+			e.Add(this);
+			entities.Add(e);
+		}
+
+		toAddAtEndOfUpdate.Clear();
+	}
+
+	private void HandleRemovedEntities() {
+		foreach (var e in toRemoveAtEndOfUpdate) {
+			e.Remove();
+			entities.Remove(e);
+		}
+
+		toRemoveAtEndOfUpdate.Clear();
+	}
+
+	public virtual void Update() {
+
+	}
+
+	/// <summary>
+	/// Updates the world and all entities within.
+	/// Do NOT override this method, override Update() instead.
+	/// </summary>
+	public void InnerUpdate() {
+
+		Update();
+
 		foreach (var e in entities) {
 			e.Update();
 		}
+
+		HandleAddedEntities();
+		HandleRemovedEntities();
 	}
 
 }
