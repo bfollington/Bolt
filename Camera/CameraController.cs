5df04260109986a4166d96a380f6d6b74d588693
Ben Follington <bfollington@gmail.com>
2015-09-06 22:36:37 +1000

    Renamed camera component

---
+++ Camera/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public float scale = 2;
+
+	// Use this for initialization
+	void Start () {
+
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		GetComponent<Camera>().orthographicSize = Screen.height / (scale * 2);
+	
+	}
+}
